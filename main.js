17d42e9dab8d40c2faaabb85a5be0981030bed80
o0nononon0o <153801808+o0nononon0o@users.noreply.github.com>
2023-12-27 18:03:19 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let num = 1;
+
+setInterval(() => {
+  document.querySelector(`#pic${num}`).classList.remove('slide');
+  if(num === 3){
+    num = 1;
+  } else{
+    num ++;
+  }
+  document.querySelector(`#pic${num}`).classList.add('slide');
+  }, 4000);
