dcbb5dc97251ac49a64d2851b73af332aa04ac0f
o0nononon0o <153801808+o0nononon0o@users.noreply.github.com>
2023-12-27 18:11:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 'use strict';
+{
+  let num = 1;
 
-let num = 1;
-
-setInterval(() => {
-  document.querySelector(`#pic${num}`).classList.remove('slide');
-  if(num === 3){
-    num = 1;
-  } else{
-    num ++;
-  }
-  document.querySelector(`#pic${num}`).classList.add('slide');
-  }, 4000);
+  setInterval(() => {
+    document.querySelector(`#pic${num}`).classList.remove('slide');
+    if(num === 3){
+      num = 1;
+    } else{
+      num ++;
+    }
+    document.querySelector(`#pic${num}`).classList.add('slide');
+    }, 4000);
+}
