7a6d5a358c7ef9dd0ca3d118ddd90c0ab30b41fb
o0nononon0o <153801808+o0nononon0o@users.noreply.github.com>
2024-01-05 14:22:35 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 'use strict';
 {
 
-  const secElements = document.querySelectorAll('section');
+  const imgElements = document.querySelectorAll('.container');
 
   window.addEventListener('scroll', () => {
-    secElements.forEach(secElement => {
-      if (secElement.getBoundingClientRect().top < window.innerHeight * 0.8) {
-        secElement.classList.add('fadein');
+    imgElements.forEach(imgElement => {
+      if (imgElement.getBoundingClientRect().top < window.innerHeight * 0.8) {
+        imgElement.classList.add('fadein');
       }
     })
+  });
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+      document.querySelector('footer a').classList.add('fade');
+      } else {
+        document.querySelector('footer a').classList.remove('fade');
+      }
   });
 
 
